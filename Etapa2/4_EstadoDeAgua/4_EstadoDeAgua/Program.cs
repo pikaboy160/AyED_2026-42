e75535d4bbfb47d7828798e0531b55aa1a4397c7
SPallares42 <santipallares07@gmail.com>
2026-04-07 16:39:53 -0300

    etapa 2

---
+++ Etapa2/4_EstadoDeAgua/4_EstadoDeAgua/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_EstadoDeAgua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese Una Cantidad de grados celcius para el agua: ");
+            int grados = int.Parse(Console.ReadLine());
+            if (grados <= 0)  
+            {
+                Console.WriteLine("Su agua Congela");
+            }
+            if (grados >= 100)
+            {
+                Console.WriteLine("su agua hierve");
+            }
+            if (grados >= 1 && grados <= 99)
+            {
+                Console.WriteLine("Su agua esta en estado liquido");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa2/5_Apocalipsis/5_Apocalipsis/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Apocalipsis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("¿tienes un refugio? (SI/NO)");
+            string refugio = Console.ReadLine();
+            Console.Write("¿tienes comida? (SI/NO)");
+            string comida = Console.ReadLine();
+            Console.Write("¿tienes algo con que defenderte? (SI/NO)");
+            string arma = Console.ReadLine();
+            Console.Write("¿tienes agua? (SI/NO)");
+            string agua = Console.ReadLine();
+            Console.Write("¿tienes Medicinas? (SI/NO)");
+            string medicina = Console.ReadLine();
+            Console.Write("¿tienes problema fisico? (SI/NO)");
+            string fisico = Console.ReadLine();
+        }
+    }
+}

1dd40dfff190d97f92f52cfab3ab3a44939adc31
SPallares42 <santipallares07@gmail.com>
2026-04-08 16:37:39 -0300

    subo etapa 2

---
+++ Etapa2/8_AnioBisiesto/8_AnioBisiesto/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_AnioBisiesto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ingrese un año: ");
+            int año = int.Parse(Console.ReadLine());
+            if (año % 4 == 0 && año % 100 != 0 || año % 400 == 0)
+            {
+                Console.WriteLine("el año ingresado es bisiesto");
+            }
+            else
+            {
+                Console.WriteLine("El año ingresado no es bisiesto");
+            }
+            Console.ReadKey();
+        }
+    }
+}
